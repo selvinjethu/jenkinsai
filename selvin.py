bc8662bb35c22bd7cf06f780dd262a81770c7748
selvinjethu <60860622+selvinjethu@users.noreply.github.com>
2022-06-06 15:06:34 +0530

    Update selvin.py

--- selvin.py
+++ selvin.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome("D:\selenium\chromedriver\chromedriver.exe")
-driver.get ("http://wikipedia.org")
+driver.get ("http://wikipedi")
 print(driver.title)
 time.sleep(8)
 driver.quit()
