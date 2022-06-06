d7c665665834e6f159c85d71c39f1249ad4ec7ab
selvinjethu <60860622+selvinjethu@users.noreply.github.com>
2022-06-06 14:57:37 +0530

    Delete selvin.py

--- selvin.py
+++
@@ -1,8 +0,0 @@
-import time
-from selenium import webdriver
-
-driver = webdriver.Chrome("/home/ubuntu/chromedriver.exe")
-driver.get ("http://wikipedia.org")
-print(driver.title)
-time.sleep(8)
-driver.quit()
